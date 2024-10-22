be55516908fbdd4ed8c9475706308f97b1ea7926
lionel <>
2024-10-22 13:54:44 +0200

    [FIX] Design

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,9 +5,10 @@ import * as zxcvbnFrPackage from "@zxcvbn-ts/language-fr";
 import { FeedbackType } from "@zxcvbn-ts/core/dist/types";
 
 import { Indicators } from "./components/Indicators";
-import { Warning, Suggestions } from "./components/Suggestions";
+import { Warning, Suggestions, Success } from "./components/Suggestions";
 
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const options = {
   translations: zxcvbnFrPackage.translations,
@@ -19,7 +20,7 @@ const options = {
   },
 };
 
-zxcvbnOptions.setOptions(options)
+zxcvbnOptions.setOptions(options);
 
 interface Indicator {
   score: number;
@@ -36,13 +37,14 @@ const getAdjustedScore = (password: string, score: number): number => {
   const uniqueCharacterSets = characterSets.filter(Boolean).length;
 
   if (uniqueCharacterSets < 3 || password.length < 12) {
-    return Math.min(score, 3);
+    return Math.min(score, 2);
   }
 
   return score;
 };
 
 const App = () => {
+  document.title = 'Password Benchmarker!';
   const [password, setPassword] = useState("");
   const [indicator, setIndicator] = useState<Indicator | null>();
 
@@ -63,25 +65,62 @@ const App = () => {
   console.log("feedback", feedback);
 
   return (
-    <div className="d-block mx-4">
-      <div className="position-relative mt-3">
-        <label htmlFor="password-input" className="mr-2">
-          Mot de passe
-        </label>
-        <input
-          is="password-input"
-          type="password"
-          onChange={(event) => setPassword(event.target.value)}
-          value={password}
-          placeholder={""}
-        />
-        {password !== "" && <Indicators score={score} />}
-        {feedback && feedback.warning && (
-          <Warning warning={feedback.warning} />
-        )}
-        {feedback && feedback.suggestions && feedback.suggestions.length > 0 && (
-          <Suggestions suggestions={feedback.suggestions} />
-        )}
+    <div className="d-flex align-items-center light-blue-gradient">
+      <div className="container">
+        <div className="d-flex justify-content-center">
+          <div className="col-md-12">
+            <div className="card rounded-0 shadow">
+              <div className="card-body">
+                <div className="d-block mx-6">
+                  <div className="position-relative mt-3">
+                  <h3>Evaluez la robustesse de votre mot de passe</h3>
+                  <h5>Un mot de passe ne protège vraiment que s'il ne peut pas être deviné.</h5>
+                    <label htmlFor="password-input" className="mr-2">
+                    </label>
+                    <form>
+                      <div className="form-group full-width-input">
+                      <ul className="list-group">
+                          <li className="list-group-item list-group-item-info">
+                            <div>En 2018, un mode de passe simple comme "Lionel123!" demandait 30 minutes pour être piraté.</div>
+                            <div><b>Aujourd'hui il ne faudrait plus que 2 secondes !</b></div>
+                          </li>
+                        </ul>
+                      <label htmlFor="password-input" className="mr-2 mt-3">
+                      Mot de passe
+                      </label>
+                        <input
+                          className="form-control full-width-input"
+                          is="password-input"
+                          type="password"
+                          onChange={(event) => setPassword(event.target.value)}
+                          value={password}
+                          placeholder={""}
+                          size={65}
+                          onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                              e.preventDefault();
+                            }
+                          }}
+                        />
+                        <div></div>
+                        {password !== "" && <Indicators score={score} />}
+                        {feedback && feedback.warning && (
+                          <Warning warning={feedback.warning} />
+                        )}
+                        {password !== "" && feedback && feedback.suggestions && feedback.suggestions.length > 0 && (
+                          <Suggestions suggestions={feedback.suggestions} />
+                        )}
+                        {password !== "" && score === 4 && (
+                          <Success success={"Félicitation ce mot de passe à l'air robuste!"} />
+                        )}
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- web/src/components/Suggestions.tsx
+++ web/src/components/Suggestions.tsx
@@ -1,18 +1,43 @@
 import React from "react";
 
-const Warning = ({ warning }: { warning: string }) => (
-  <ul>
-    <li key={warning}>{warning}</li>
+const Success = ({ success }: { success: string }) => (
+  <ul className="list-group">
+    <li className="list-group-item list-group-item-success" key={success}>🏅 - {success}</li>
   </ul>
 );
 
-const Suggestions = ({ suggestions }: { suggestions: string[] }) => (
-  <ul>
-    {suggestions.map((suggestion, index) => (
-      <li key={suggestion}>{suggestion}</li>
-    ))}
+const Warning = ({ warning }: { warning: string }) => (
+  <ul className="list-group">
+    <li className="list-group-item list-group-item-danger" key={warning}>🚫 - {warning}</li>
   </ul>
 );
+
+const Suggestions = ({ suggestions }: { suggestions: string[] }) => {
+  const transformSuggestions = (suggestions: string[]): string[] => {
+    return suggestions.map(suggestion => {
+      if (suggestion === "Ajoutez des mots moins courants.") {
+        return "Ce mot de passe n'est pas suffisamment complexe.";
+      }
+      if (suggestion === "Capitalisez mais pas seulement la première lettre.") {
+        return "Une majuscule c'est bien, mais pas seulement pour la première lettre.";
+      }
+      return suggestion;
+    });
+  };
+
+  const transformedSuggestions = transformSuggestions(suggestions);
+
+  return (
+    <ul className="list-group">
+      {transformedSuggestions.map((suggestion, index) => (
+        <li className="list-group-item list-group-item-warning" key={index}>
+          ツ - {suggestion}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export { Warning };
 export { Suggestions };
+export { Success };
